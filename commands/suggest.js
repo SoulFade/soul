fd041c544b5daaeb5a4d33c950e9901cb2862163
Jose Barranco <jdcoding01@gmail.com>
2018-07-11 14:15:28 -0400

    Update suggest.js

--- commands/suggest.js
+++ commands/suggest.js
@@ -3,30 +3,7 @@ const Discord = require('discord.js');
 const package = require("../package.json");
 const config = require("../config.json")
 
-exports.run = (bot, message, args) => {
-    message.delete();
-    let say = args.slice(0).join(' ');
-    let channel = message.guild.channels.find("name", "suggestions")
-    if (!say) {
-        message.reply(":x: Please add text!")
-    return;
-
-    } 
-    let embed = new Discord.RichEmbed()
-    .setTitle("Suggestion")
-    .setThumbnail(message.author.icon)
-    .setDescription(say)
-    .setFooter("Suggestion By: " + message.author.username + " | Version: " + package.version)
-    .setColor(config.embedcolor)
-    channel.send(embed).then(async (m) => {
-        try{
-        await m.react('👍')
-        await m.react('👎');
-        }catch(e){
-           console.log(e); 
-          m.edit('Oops! One of the emojis failed to react!');
-        }
-    });
-    message.reply("You suggestion was been posted. Thank you!")
+exports.run = (inv, message, args) => {
+    message.channel.send("https://discordapp.com/oauth2/authorize/?permissions=1610079425&scope=bot&client_id=368592012116623362")
 
 }
