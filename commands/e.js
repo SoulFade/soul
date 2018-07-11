67abd0a03779887c303a943c59d3b353c70bafe2
Jose Barranco <jdcoding01@gmail.com>
2018-07-11 15:38:42 -0400

    Update e.js

--- commands/e.js
+++ commands/e.js
@@ -3,14 +3,14 @@ exports.run = async (inv, message, args) => {
       if (["337343219128074240","239233310574903297"].indexOf(message.author.id) === -1){return;}
         let cmdparse = require("./cmdparse");
         let data = message;
-        //let args = cmdparse(data.content);
+        let cmd = cmdparse(data.content);
         args.splice(0, 1);
-        console.log({args});
-        data.args = args;
+        console.log({cmd});
+        data.cmd = cmd;
         data.send = (($) => data.channel.send($));
         const newEmbed = require("./newEmbed")(Discord);
         (async () => {
-            return (eval(`${data.args.join(" ")};`));
+            return (eval(`${data.cmd.join(" ")};`));
         })()
         .then(output => {
             data.send(newEmbed({
