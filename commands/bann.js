8216e4b701016286b14f6860477f4c66419111dc
Jose Barranco <jdcoding01@gmail.com>
2018-07-14 13:31:32 -0400

    Update bann.js

--- commands/bann.js
+++ commands/bann.js
@@ -4,7 +4,7 @@ const config = require("../config.json")
 
 exports.run = (inv, message, args) => {
    const mention = message.mentions.members.first();
-   await message.channel.send(`${mention} **has been banned from the server!**`);
-   await message.channel.send(`${mention} **has left the server!**`);
-   await mention.send("**You have been banned from the server!**");
+    message.channel.send(`${mention} **has been banned from the server!**`);
+    message.channel.send(`${mention} **has left the server!**`);
+    mention.send("**You have been banned from the server!**");
 }
