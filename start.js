f38cf954050cdb84dee0cd62ecb4d8098a7df0e2
Jose Barranco <jdcoding01@gmail.com>
2018-07-15 17:32:25 -0400

    Update start.js

--- start.js
+++ start.js
@@ -1,51 +1,92 @@
-const Discord = require("discord.js");
 const config = require("./config.json");
 const package = require("./package.json");
+const Discord = require('discord.js');
 const fs =require("fs");
 const figlet = require('figlet');
 const prefix = config.prefix;
 const inv = new Discord.Client();
-const talkedRecently = new Set();
+
+//Logs of readyness
 inv.on('ready', () => {
-  inv.user.setPresence({game:{name: `development`}}).catch(console.error);
+  inv.user.setPresence({game:{name: config.prefix + "help | V." + package.version ,type:1}}).catch(console.error);
   inv.user.setStatus("online").catch(console.error);
+
+  figlet('GD', function(err, data) {
+    if (err) {
+        console.log('Something went wrong...');
+        console.dir(err);
+        return;
+    }
+    console.log(data)
+    console.log("")
+    console.log("")
+    console.log("Core by")
+    figlet('Alphi', function(err, data) {
+      if (err) {
+          console.log('Something went wrong...');
+          console.dir(err);
+          return;
+      }
+      console.log(data)
+        figlet('==========', function(err, data) {
+          if (err) {
+              console.log('Something went wrong...');
+              console.dir(err);
+              return;
+          }
+          console.log(data)
+        });
+      });
+    });
+});
+
+
 //cmd handler
 fs.readdir("./events/", (err, files) => {
     if (err) return console.error(err);
     files.forEach(file => {
       let eventFunction = require(`./events/${file}`);
       let eventName = file.split(".")[0];
-      inv.on(eventName, (...args) => eventFunction.run(inv, ...args));
-    }
+      bot.on(eventName, (...args) => eventFunction.run(bot, ...args));
+    });
   });
-});
- 
+
 inv.on("message", message => {
   if (message.author.bot) return;
   if(message.content.indexOf(config.prefix) !== 0) return;
   const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
-  const prefixMention = new RegExp(`^<@!?${inv.user.id}> `);
-    const prefix = message.content.match(prefixMention) ? message.content.match(prefixMention)[0] : config.prefix;
   const command = args.shift().toLowerCase();
-
-
+  try {
+    if (message.channel.type === "dm") {
+      let embed1 = new Discord.RichEmbed() //info embed on ticket
+        .setTitle("Error :x:")
+        .setColor("db1212")
+        .setDescription("DM Commands are not allowed!")
+        .setFooter(`Version ` + package.version)
+      message.channel.send(embed1)
+      return;
+  }
     let commandFile = require(`./commands/${command}.js`);
     commandFile.run(inv, message, args);
-  });
+  } catch (err) {
 
+    console.error(err);
+  }
 });
-inv.on("guildMemberAdd", async member => {
+
+inv.on("guildMemberAdd", member => {
     let wchan =  member.guild.channels.find("name", "welcome");
-    let people = member.guild.memberCount 
+    let people = member.guild.memberCount - 4
   
     let embed = new Discord.RichEmbed() //info embed on ticket
        .setTitle("**Member Joined**")
        .setColor('GREEN')
-       .setDescription("Welcome " + member + " to " + member.guild.name + "!\nMember count " + people)
-       .setThumbnail(member.user.displayAvatarURL)
-       .setFooter(`| Made by Inv Technologies | ${member.guild.name}`, member.guild.iconURL)
-        wchan.send({embed})
-        wchan.send(`Hi ${member.toString()}! Please respond with 'agree' to access the server.`);
+ .setDescription("Welcome " + member + " to " + member.guild.name + "!\nMember count " + people)
+ .setThumbnail(member.user.displayAvatarURL)
+ .setFooter(`| Made by Inv Technologies | ${member.guild.name}`, member.guild.iconURL)
+
+    wchan.send(embed)
+   wchan.send(`Hi ${member.toString()}! Please respond with 'agree' to access the server.`);
         let response =  await wchan.awaitMessages(mg => {
         return mg.author.id === member.id;
     }, {max: 1})
@@ -58,18 +99,20 @@ inv.on("guildMemberAdd", async member => {
     }
     else{
         wchan.send("You did not meet the requirements, please contact an admin or try again.");
-    }
+    })
   });
-//hey
+
 inv.on("guildMemberRemove", member => {
   let wchan =  member.guild.channels.find("name", "welcome");
-    let people = member.guild.memberCount   
+    let people = member.guild.memberCount - 4
+  
     let embed = new Discord.RichEmbed() //info embed on ticket
        .setTitle("**Member Left**")
        .setColor('GREEN')
-       .setDescription("Good bye" + member + " you will be missed! We now have \nMember count! `" + people + "`")
-       .setThumbnail(member.user.displayAvatarURL)
-        .setFooter('| Made By Inv Technologies', member.guild.iconURL)
-         wchan.send(embed)
-  })
-inv.login(process.env.token); 
+ .setDescription("Good bye" + member + " you will be missed! We now have \nMember count! `" + people + "`")
+ .setThumbnail(member.user.displayAvatarURL)
+ .setFooter('| Made By Inv Technologies', member.guild.iconURL)
+    wchan.send(embed)
+  });
+
+  inv.login(process.env.token);
