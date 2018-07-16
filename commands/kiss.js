6333f273c3e56a6be3a1611044047a70ac9c3473
Jose Barranco <jdcoding01@gmail.com>
2018-07-16 12:29:53 -0400

    Update kiss.js

--- commands/kiss.js
+++ commands/kiss.js
@@ -6,6 +6,7 @@ exports.run = (inv, message, args) => {
    const mention = message.mentions.members.first();
     const kiss = new Discord.RichEmbed()
     .setTimestamp()
+    .setColor(0x005500)
     .setImage("http://www.mobiletoones.com/downloads/wallpapers/love_wallpapers/preview/25/64432-kiss.jpg")
     message.channel.send(`${mention}` +  "**, you have been kissed by: ** " + message.author.username + " :heart:" );
     message.channel.send(kiss);
