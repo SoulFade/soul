4348d069a7bea1a3c18f4815bcd5554725ebeb30
Jose Barranco <jdcoding01@gmail.com>
2018-07-16 19:10:11 -0400

    Update kiss.js

--- commands/kiss.js
+++ commands/kiss.js
@@ -1,14 +1,19 @@
 const Discord = require('discord.js');
 const package = require("../package.json");
 const config = require("../config.json")
-
+let image = Math.floor((Math.random() * images.length))
+let images = "../kiss_images"
 exports.run = (inv, message, args) => {
    const mention = message.mentions.members.first();
     const kiss = new Discord.RichEmbed()
     .setTimestamp()
     .setColor(0x005500)
-    .setImage("http://www.mobiletoones.com/downloads/wallpapers/love_wallpapers/preview/25/64432-kiss.jpg")
+    .setImage(images[image])
     message.channel.send(`${mention}` +  "**, you have been kissed by: ** " + message.author.username + " :heart:" );
     message.channel.send(kiss);
     mention.send("**You have been kissed by: **" + message.author.username + " in: " + message.guild.name);
+   
+let image = Math.floor((Math.random() * images.length))
+
+
 }
