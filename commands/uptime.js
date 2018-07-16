68a5cea3f6795d427d954b2a9577fc9fecb120e0
Jose Barranco <jdcoding01@gmail.com>
2018-07-16 16:14:38 -0400

    Update uptime.js

--- commands/uptime.js
+++ commands/uptime.js
@@ -4,7 +4,7 @@ exports.run = (inv, message, args) => {
     .setAuthor("Vell's Uptime", inv.user.avatarURL)
     .setDescription("Uptime of Vell Bot")
     .addField("Hours",Math.round(inv.uptime / (1000 * 60 * 60)), true )
-    .addField("Minutes", Math.round(inv.uptime / (1000 * 60)) % 60, true);
+    .addField("Minutes", Math.round(inv.uptime / (1000 * 60)) % 60, true)
     .setFooter(`Vell is currently serving ${inv.users.size} users!`, message.author.avatarURL)
      message.channel.send(uptime)
     }
