9ad4990d03f92f3ec0478fd2fc7c3c01ead5ef4b
Odar <radoradev770@gmail.com>
2018-07-28 01:27:16 +0300

    Update submit.js

--- commands/submit.js
+++ commands/submit.js
@@ -2,7 +2,7 @@ const discord = requre("discord.js")
 
 exports.run = (inv, message, args) => {
         
-        if (command === "submit") {
+  
         if (message.channel.id !== "465277567491112972") return message.channel.send("You can use this command only in #staff-application");
         let chan = inv.channels.get("465277567491112972");
         let chan2 = inv.channels.get("465269674389667871");
