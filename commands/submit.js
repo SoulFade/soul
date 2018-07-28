fa440fd336f79b3d545724f15b9cb0c7ec82757e
Odar <radoradev770@gmail.com>
2018-07-28 14:20:54 +0300

    Update submit.js

--- commands/submit.js
+++ commands/submit.js
@@ -7,7 +7,7 @@ exports.run = (inv, message, args) => {
         let chan = inv.channels.get("465277567491112972");
         let chan2 = inv.channels.get("465269674389667871");
         let msg = args.join(" ")
-        if (!msg) return message.reply("Please tell us for what are you applying!");
+        if (!msg) return message.reply("Please add an application description before using the command. Example: ?submit I am awesome");
 
         chan.send(`${message.author} Application Enqueued. Please Wait.`);
         
