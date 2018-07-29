14beeefc285728c7825ee620b7abf225adae18ab
Jose Barranco <jdcoding01@gmail.com>
2018-07-29 13:28:18 -0400

    Update google.js

--- commands/google.js
+++ commands/google.js
@@ -6,7 +6,7 @@ const package2 = require("../package.json")
 
 exports.run = async (inv, msg, args) => {
     if(args.length < 1) msg.channel.send('I need to know what to search...');
-    //msg.react(inv.emojis.get("473173054252384258"))
+    await msg.react(inv.emojis.get("473173054252384258"))
     await msg.channel.send('<a:loading:465944291634839554> Googling....').then(msg => {msg.delete(1000)});
 
     const params = {
