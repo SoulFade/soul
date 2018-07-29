2f5f0c3ae0be61915dccbe1c7163f4526bec87ca
Jose Barranco <jdcoding01@gmail.com>
2018-07-29 12:58:38 -0400

    Update botinfo.js

--- commands/botinfo.js
+++ commands/botinfo.js
@@ -2,7 +2,7 @@ const discord = require('discord.js');
 const config = require('../config.json')
 
 exports.run = (inv, message, params) => {
-
+    message.react("✅")
     var botinfo = new discord.RichEmbed()
         .setTitle(`Vell Pro`)
         .setAuthor(`Author: Alphi#9839`, "https://cdn.discordapp.com/avatars/368592012116623362/3d1b0bf15c53f0d29c57430588bd4cdb.jpg?size=1024")
