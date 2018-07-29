d4d68ce62f0646693e176e1e72374e9cfe886ac5
Jose Barranco <jdcoding01@gmail.com>
2018-07-29 13:22:07 -0400

    Update google.js

--- commands/google.js
+++ commands/google.js
@@ -5,7 +5,6 @@ const Discord = require("discord.js");
 const package2 = require("../package.json")
 
 exports.run = async (inv, msg, args) => {
-    msg.react(inv.emojis.get("473173054252384258"))
     if(args.length < 1) msg.channel.send('I need to know what to search...');
 
     await msg.channel.send('<a:loading:465944291634839554> Googling....').then(msg => {msg.delete(1000)});
@@ -59,5 +58,5 @@ exports.run = async (inv, msg, args) => {
     const resultxD = results.slice(0, 1).map(r => `${r.link}`).join('\n');
 
     await msg.channel.send(resultxD);
-
+    msg.react(inv.emojis.get("473173054252384258"))
 };
