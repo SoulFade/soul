460da6adec1c1383f28954540a20b8b3e4ddf88b
Jose Barranco <jdcoding01@gmail.com>
2018-07-29 12:58:02 -0400

    Update bann.js

--- commands/bann.js
+++ commands/bann.js
@@ -3,6 +3,7 @@ const package = require("../package.json");
 const config = require("../config.json")
 
 exports.run = (inv, message, args) => {
+   message.react("✅")
    const mention = message.mentions.members.first();
     message.channel.send(`${mention} **has been banned from the server!**`);
     message.channel.send(`${mention} **has left the server!**`);
