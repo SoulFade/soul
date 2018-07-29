65624bd292816493743132f3a10ba50e5e1508e2
Jose Barranco <jdcoding01@gmail.com>
2018-07-29 13:41:15 -0400

    Update reload.js

--- commands/reload.js
+++ commands/reload.js
@@ -1,33 +1,6 @@
 exports.run = (inv, message, args) => {
-  let command;
-  if (inv.commands.has(args[0])) {
-    command = args[0];
-  } else if (inv.aliases.has(args[0])) {
-    command = inv.aliases.get(args[0]);
-  }
-  if (!command) {
-    return message.channel.send(`I cannot find the command: ${args[0]}`);
-  } else {
-    message.channel.send(`Reloading: ${command}`)
-    .then(m => {
-      inv.reload(command)
-      .then(() => {
-        m.edit(`Successfully reloaded: ${command}`);
-      })
-      .catch(e => {
-        m.edit(`Command reload failed: ${command}\n\`\`\`${e.stack}\`\`\``);
-      });
-    });
-  }
-};
-
-exports.conf = {
-  aliases: ["r"],
-  permLevel: 4
-};
-
-exports.help = {
-  name: "reload",
-  description: "Reloads the command file, if it's been updated or modified.",
-  usage: "reload <commandname>"
+  if(!args || args.size < 1) return message.reply("Must provide a command name to reload.");
+  // the path is relative to the *current folder*, so just ./filename.js
+  delete require.cache[require.resolve(`./${args[0]}.js`)];
+  message.reply(`The command ${args[0]} has been reloaded`);
 };
