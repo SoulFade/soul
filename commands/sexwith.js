dab2ed05c8fb64a3dbe3c7d3058e6396156efbdc
Jose Barranco <jdcoding01@gmail.com>
2018-08-01 14:10:26 -0400

    Update sexwith.js

--- commands/sexwith.js
+++ commands/sexwith.js
@@ -1,5 +1,5 @@
 const Discord = require('discord.js');
-exports.run = (inv, message, args) => {
+exports.run = async (inv, message, args) => {
 message.react("👉")
 message.react("👌")
 message.react("🔞")
